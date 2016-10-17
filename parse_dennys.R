798512b8d0e310f09eb2829838bd0aa00a2133b6
Faustine Li <faustineli12@gmail.com>
2016-10-16 20:35:09 -0400

    download dennys data, parse, and save data.

--- parse_dennys.R
+++ parse_dennys.R
@@ -1,4 +1,38 @@
-dennys=1
+library(rvest)
+library(stringr)
+library(tibble)
+suppressMessages(library(dplyr))
+
+files = dir("data/dennys/", "xml", full.names = TRUE)
+res = list()
+
+# parse the dennys xml files and extract information
+for (i in seq_along(files)) {
+
+    file = files[i]
+    page = read_xml(file)
+    # extract latitude information
+    lat = page %>%
+        html_nodes("latitude") %>%
+        html_text()
+    # extract longitude information
+    long = page %>%
+        html_nodes('longitude') %>%
+        html_text()
+    # extract state information
+    state = page %>%
+        html_nodes('state') %>%
+        html_text()
+    # store information in a list format
+    res[[i]] = data_frame(
+        state = state,
+        lat   = lat,
+        long  = long
+    )
+}
+
+dennys = bind_rows(res)
+dennys = dennys %>% distinct()
 
 dir.create("data/",showWarnings = FALSE)
 save(dennys,file="data/dennys.Rdata")
--- parse_lq.R
+++ parse_lq.R
@@ -3,44 +3,42 @@ library(stringr)
 library(tibble)
 library(dplyr)
 
+# intialize
 files = dir("data/lq/", "html", full.names = TRUE)
-
 res = list()
 
-for(i in seq_along(files))
-{
-  file = files[i]
-  
-  page = read_html(file)
-  
-  hotel_info = page %>% 
-    html_nodes(".hotelDetailsBasicInfoTitle p") %>%
-    html_text() %>% 
-    str_split("\n") %>% 
-    .[[1]] %>% 
-    str_trim() %>%
-    .[. != ""]
-
-  n_rooms = page %>% 
-    html_nodes(".hotelFeatureList li:nth-child(2)") %>%
-    html_text() %>%
-    str_trim() %>%
-    str_replace("Rooms: ", "") %>%
-    as.integer()
-  
-  # Google link includes latitude first then longitude
-  lat_long = page %>%
-    html_nodes(".minimap") %>%
-    html_attr("src") %>%
-    str_match("\\|(-?[0-9]{1,2}\\.[0-9]+),(-?[0-9]{1,3}\\.[0-9]+)&")
-
-  res[[i]] = data_frame(
-    address = paste(hotel_info[1:2],collapse="\n"),
-    phone = hotel_info[3] %>% str_replace("Phone: ", ""), 
-    fax   = hotel_info[4] %>% str_replace("Fax: ", ""),
-    n_rooms = n_rooms,
-    lat   = lat_long[,2],
-    long  = lat_long[,3]
+# read files and extract information
+for(i in seq_along(files)) {
+    file = files[i]
+    page = read_html(file)
+    # extract address infp
+    hotel_info = page %>%
+        html_nodes(".hotelDetailsBasicInfoTitle p") %>%
+        html_text() %>%
+        str_split("\n") %>%
+        .[[1]] %>%
+        str_trim() %>%
+        .[. != ""]
+    # extract number of rooms
+    n_rooms = page %>%
+        html_nodes(".hotelFeatureList li:nth-child(2)") %>%
+        html_text() %>%
+        str_trim() %>%
+        str_replace("Rooms: ", "") %>%
+        as.integer()
+    # Google link includes latitude first then longitude
+    lat_long = page %>%
+        html_nodes(".minimap") %>%
+        html_attr("src") %>%
+        str_match("\\|(-?[0-9]{1,2}\\.[0-9]+),(-?[0-9]{1,3}\\.[0-9]+)&")
+    # store infomation in list structure
+    res[[i]] = data_frame(
+        address = paste(hotel_info[1:2],collapse="\n"),
+        phone = hotel_info[3] %>% str_replace("Phone: ", ""),
+        fax   = hotel_info[4] %>% str_replace("Fax: ", ""),
+        n_rooms = n_rooms,
+        lat   = lat_long[,2],
+        long  = lat_long[,3]
   )
 }
 
