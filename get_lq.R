00a6067199f292bcc24066fd5d9c59cbbfe1e52d
Yumemichi Fujita <yf60@saxon.stat.duke.edu>
2016-10-18 12:32:54 -0400

    updated

--- get_lq.R
+++ get_lq.R
@@ -1,23 +1,31 @@
 library(rvest)
+library(dplyr)
 
 # url for la quinta site
 site = "http://www2.stat.duke.edu/~cr173/lq_test/www.lq.com/en/findandbook/"
 url = paste0(site,"hotel-listings.html")
 
 # extract individual hotel page urls
+#go laquinta website, download all the information
+#first get all of the hotels regarding the location
+#download all of the individual websites
 page = read_html(url)
 hotel_pages = page %>%
   html_nodes("#hotelListing .col-sm-12 a") %>%
   html_attr("href") %>%
-  na.omit()
+  na.omit()#strip out the NA
 
 # create directory
-dir.create("data/lq",recursive = TRUE,showWarnings = FALSE)
+dir.create("data/lq",recursive = TRUE,showWarnings = FALSE)# create a directory
+#showWarnings = don't show the warning
+#give the path I want; data/lq
 
 # download the hotel page for each lq location
 for(hotel_page in hotel_pages) {
   hotel_url = paste0(site, hotel_page)
   download.file(url = hotel_url,
-                destfile = file.path("data/lq",hotel_page),
+                destfile = file.path("data/lq",hotel_page),#destination file
+                #need to put the destfile in the directory, use filepath function, add one /
                 quiet = TRUE)
 }
+
